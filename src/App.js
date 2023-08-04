37011771ea5dff757c57ba00e306ad02ece74f5d
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-04 09:07:52 -0300

    Continue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Topbar from './Topbar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
   const [busqueda, setBusqueda] = useState("");
-  
+  const [productos, setProductos] = useState([{}]);
+
+  const fetchProductos = async () => {
+    const resp = await axios.get("https://dummyjson.com/products?limit=150");
+    setProductos(resp.data.products);
+    console.log(resp.data.products);
+  }
+
+  useEffect(() => {
+    fetchProductos();
+  }, [])
+
   function actualizarBusqueda(e) {
     if(e.keycode == 13) { //enter
       return;
@@ -13,13 +26,20 @@ function App() {
     setBusqueda(e.target.value)
   }
 
+  if(productos.length === 0) return (<></>);
+
   return (
-    <div>
-      <Topbar busqueda={busqueda} actualizarBusqueda={actualizarBusqueda}/>
-      <div>
-        <h>lol</h>
-      </div>
-    </div>
+    <>
+    <Topbar busqueda={busqueda} actualizarBusqueda={actualizarBusqueda}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<div>lol</div>}></Route>
+          <Route path="/productos" element={<div>lmao</div>}></Route>
+          <Route path="/detalle" element={<div>noway</div>}></Route>
+          <Route path="/contacto" element={<div>nomellamen</div>}></Route>  
+        </Routes>
+      </BrowserRouter>
+      </>
   );
 }
 
